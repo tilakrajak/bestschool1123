1c14cad19fa18f9cc984ce1df7ce204e5c6483dd
tilakrajak <141298166+tilakrajak@users.noreply.github.com>
2023-08-29 08:08:27 -0700

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-const Contactschema = new mongoose.Schema({
+const ContactSchema = new mongoose.Schema({
     name:{
         type:String,
         require:true
@@ -19,6 +19,6 @@ const Contactschema = new mongoose.Schema({
 
 },{timestamps:true})
 
-const ContactModel = mongoose.model('contact',Contactschema)
+const ContactModel = mongoose.model('contact',ContactSchema)
 
 module.exports=ContactModel
