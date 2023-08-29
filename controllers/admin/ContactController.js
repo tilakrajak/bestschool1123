a1fea09bb307c858d20b45be3c5b073fcb826716
tilakrajak <141298166+tilakrajak@users.noreply.github.com>
2023-08-29 08:15:26 -0700

    Update ContactController.js

--- controllers/admin/ContactController.js
+++ controllers/admin/ContactController.js
@@ -2,17 +2,17 @@ const ContactModel = require("../../models/contact");
 
 class ContactController{
 
-    static addcontact = async(req,res)=>{
-        try{
-           // res.render('admin/contact/addcontact')
-           const{name,image}= req.data1
-            const data = await ContactModel.find()
-            console.log(data);
-            res.render('admin/contact/addcontact',{d:data,n:name,img:image})
-        }catch (error) {
-            console.log(error)
-        }
-    }
+    // static addcontact = async(req,res)=>{
+    //     try{
+    //        // res.render('admin/contact/addcontact')
+    //        const{name,image}= req.data1
+    //         const data = await ContactModel.find()
+    //         console.log(data);
+    //         res.render('admin/contact/addcontact',{d:data,n:name,img:image})
+    //     }catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
     static insertcontact = async(req,res)=>{
         try{       
@@ -31,48 +31,48 @@ class ContactController{
             console.log(error);
         }
     }
-    static viewcontact =async(req,res)=>{
-        try{
-            const{name,image}= req.data1
-           const data = await ContactModel.findById(req.params.id)
-           res.render('admin/contact/view',{d:data,n:name,img:image})
-        }catch (error) {
-            console.log(error);
-        }
-    }
-    static editcontact =async(req,res)=>{
-        try{
-            const{name,image}= req.data1
-           const data = await ContactModel.findById(req.params.id)
-           res.render('admin/contact/edit',{d:data,n:name,img:image})
-        }catch (error) {
-            console.log(error);
-        }
-    }
-    static updatecontact = async (req, res) => {
-        try {
-            const id =req.params.id
-            const data = await ContactModel.findByIdAndUpdate(id, {
-                name: req.body.name,
-                email: req.body.email,
-                description:req.body.description
+    // static viewcontact =async(req,res)=>{
+    //     try{
+    //         const{name,image}= req.data1
+    //        const data = await ContactModel.findById(req.params.id)
+    //        res.render('admin/contact/view',{d:data,n:name,img:image})
+    //     }catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+    // static editcontact =async(req,res)=>{
+    //     try{
+    //         const{name,image}= req.data1
+    //        const data = await ContactModel.findById(req.params.id)
+    //        res.render('admin/contact/edit',{d:data,n:name,img:image})
+    //     }catch (error) {
+    //         console.log(error);
+    //     }
+    // }
+    // static updatecontact = async (req, res) => {
+    //     try {
+    //         const id =req.params.id
+    //         const data = await ContactModel.findByIdAndUpdate(id, {
+    //             name: req.body.name,
+    //             email: req.body.email,
+    //             description:req.body.description
 
                 
-            })
-            res.redirect('/admin/addcontact')
+    //         })
+    //         res.redirect('/admin/addcontact')
 
 
 
-        } catch (erroe) {
-            console.log(error)
-        }
+    //     } catch (erroe) {
+    //         console.log(error)
+    //     }
 
-    }
+    // }
     static deletecontact =async(req,res)=>{
         try{
             const id = req.params.id
            const data = await ContactModel.findByIdAndDelete(id)
-           res.redirect('/admin/addcontact')
+           res.redirect("/admin/contactdisplay")
           
         }catch (error) {
             console.log(error);
